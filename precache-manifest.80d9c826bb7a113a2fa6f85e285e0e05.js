7ead72d50c6d59fd7a194b2aab9f78920a51e56f
Sarah Arnold <digipet007@gmail.com>
2019-12-14 12:11:17 -0800

    Updates

---
+++ precache-manifest.80d9c826bb7a113a2fa6f85e285e0e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b52c1fa28e6024aa57da19edd0f933b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f90384fde72959730d84",
+    "url": "/clicky-game/static/css/main.ac9e3254.chunk.css"
+  },
+  {
+    "revision": "00d61d0bbab4af0d0cec",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.59c7650b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f90384fde72959730d84",
+    "url": "/clicky-game/static/js/main.b74e5a63.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
